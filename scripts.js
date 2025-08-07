cdcdc12785ddb36cd8481d7c80e04fd50b7fea9e
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-06 21:35:25 -0500

    Add number and operator handling and operation

--- scripts.js
+++ scripts.js
@@ -1,9 +1,58 @@
-const calcDisplay = document.querySelector(".calculator__display");
-const calcButtons = document.querySelector(".calculator__controls");
+const calculatorDisplay = document.querySelector(".calculator__display");
+const calculatorButtons = document.querySelector(".calculator__controls");
 
-calcButtons.addEventListener("click", (e) => {
-  calcDisplay.textContent += e.target.value;
+let inputArray = [];
+let firstValue = "";
+let secondValue = "";
+let operator = "";
+let haveOperator = false;
+
+// MAIN FUNCTIONS
+
+calculatorButtons.addEventListener("click", (e) => {
+  calculatorDisplay.textContent += e.target.value;
+
+  handleNumberInput(e.target.value);
+  handleNonNumberInput(e.target.value);
 });
+
+function handleNumberInput(input) {
+  if (!isNaN(input)) {
+    if (haveOperator === false) {
+      inputArray.push(input);
+      firstValue += input;
+    } else {
+      inputArray.push(input);
+      secondValue += input;
+    }
+  }
+}
+
+function handleNonNumberInput(input) {
+  if (isNaN(input)) {
+    if (haveOperator === false) {
+      operator = input;
+      haveOperator = true;
+    } else {
+      console.log(operate(+firstValue, +secondValue, operator));
+    }
+  }
+}
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
+
+// HELPERS
 
 function add(a, b) {
   return a + b;
@@ -21,15 +70,10 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(a, b, operator) {
-  switch (operator) {
-    case "add":
-      return add(a, b);
-    case "subtract":
-      return subtract(a, b);
-    case "multiply":
-      return multiply(a, b);
-    case "divide":
-      return divide(a, b);
-  }
-}
+getIndexOfOperator = (array) => {
+  array.some((element, index) => {
+    if (isNaN(element)) {
+      return index;
+    }
+  });
+};
